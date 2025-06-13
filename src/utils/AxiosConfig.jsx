2140fe76147b5938e84c615f0b60af1e76e5412a
Sumit Bhardwaj <djadamnoob9568@gmail.com>
2025-06-12 19:43:24 -0700

    added the accept friend request in the frontend

--- src/utils/AxiosConfig.jsx
+++ src/utils/AxiosConfig.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VITE_BACKEND_URL, // Replace with your backend base URL
+  baseURL: "https://chatappbackend-c2bq.onrender.com", // Replace with your backend base URL
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
