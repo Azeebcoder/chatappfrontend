bf0502579bb80790b3f11ada552e620e8019cb8b
Sumit Bhardwaj <djadamnoob9568@gmail.com>
2025-06-16 05:31:34 -0700

    added the environmet variable for the connection of backend and frontend

--- src/utils/AxiosConfig.jsx
+++ src/utils/AxiosConfig.jsx
@@ -2,8 +2,9 @@
 import axios from "axios";
 
 
+
 const axiosInstance = axios.create({
-  baseURL: "https://chatappbackend-c2bq.onrender.com", // Replace with your backend base URL
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`, // Replace with your backend base URL
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
